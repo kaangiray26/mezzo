a9ff25358a6cbee4aa813772ec39758ac7eebe55
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-08-13 09:02:41 +0200

    flatpak

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mezzo
 Music player designed to function like major streaming platforms
+
+## development
+Build the flatpak with the following command:
+```
+flatpak-builder --force-clean --install-deps-from=flathub build-dir manifest.yml
+```
+
+Test the app:
+```
+flatpak-builder --run build-dir manifest.yml python3 /app/wsgi.py
+```
+
+dependencies:
+```
+python3 -m pip wheel -r requirements.txt -w build-dir/files/lib/python3.8/site-packages
+```
--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,19 @@
 from gevent.pywsgi import WSGIServer
 from app import app
+import webbrowser
 
 address = "localhost"
 port = 5000
 
 # Start the server
 try:
+    print(f"Server starting at http://{address}:{port}")
     http_server = WSGIServer((address, port), app)
+    http_server.start()
+    webbrowser.open("http://localhost:5000")
     http_server.serve_forever()
+
+    # http_server.serve_forever()
 except KeyboardInterrupt:
     print("Server stopped.")
     exit(0)
